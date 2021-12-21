f98218256a2af6396428d2ab3ef91c8f61c0a682
Fatih20 <fnri39@protonmail.com>
2021-12-21 15:45:50 +0700

    Make calculation result and the setting of it available to the whole body

--- src/components/body.js
+++ src/components/body.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import styled from "styled-components";
 
 import Form from "./form";
 import Result from "./result";
 
+const CalculationContext = React.createContext();
+
+export function useCalculationContext(){
+    return useContext(CalculationContext);
+}
+
 const Main = styled.div`
 `;
 
+
+
 export default function Body () {
+    const[calculation, setCalculation] = useState(null);
     return (
         <Main>
-            <Form />
-            <Result />
+            <CalculationContext.Provider value={[calculation, setCalculation]}>
+                <Form />
+                <Result />
+            </CalculationContext.Provider> 
         </Main>
     )
 }
