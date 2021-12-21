e5e01110b1bfd270e64ab12b307c801dc60c6044
Fatih20 <fnri39@protonmail.com>
2021-12-21 15:41:04 +0700

    Header fixed

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const Main = styled.div`
+    align-items: center;
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+const Title = styled.h1`
 `;
 
 export default function Header () {
     return (
         <Main>
-
+            <Title>BMI Calculator</Title>
         </Main>
     )
 }
