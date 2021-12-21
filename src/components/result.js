283dd3737d1099d76f26d5baacf4d9ac774c57b4
Fatih20 <fnri39@protonmail.com>
2021-12-21 22:30:58 +0700

    Even more work to result

--- src/components/result.js
+++ src/components/result.js
@@ -15,6 +15,21 @@ const Index = styled.h2`
     font-size: 72px;
 `;
 
+const BodyTypeContainer = styled.div`
+    align-items: end;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+
+    & > *:nth-child(2){
+        font-size: 36px;
+    }
+`;
+
+const BodyType = styled.h2`
+    color: ${({color}) => color};
+`;
+
 const ChartContainer = styled.div`
     display: ${({show}) => show ? "block" : "none"};
 `;
@@ -76,6 +91,31 @@ export default function Result () {
         },
     }
 
+    function bodyTypeAround(centerBodyType) {
+        const listOfBodyType = Object.keys(dataOfBodyType);
+        const indexOfCenter = listOfBodyType.findIndex((element) => element === centerBodyType);
+        const listOfIndex = [indexOfCenter-1, indexOfCenter, indexOfCenter+1].filter(index => index >= 0 && index < listOfBodyType.length);
+        return listOfIndex.map((index) => listOfBodyType[index])
+    }
+
+    function renderBodyType (includedBodyType){
+        return (
+            <BodyType id={includedBodyType} color={dataOfBodyType[includedBodyType]["color"]}>{includedBodyType}</BodyType>
+        )
+    }
+
+    function dataProducer (givenBodyType){
+        const value = [dataOfBodyType[bodyType]["lower"]-2, 0, dataOfBodyType[bodyType]["upper"]+2];
+        const nameOfValue = ["bottom", "middle", "upper"]
+        return (
+            Array.from(bodyTypeAround(givenBodyType), (includedBodyType, index) => {
+            return {"name" : includedBodyType}
+        }).forEach((object, index) => {
+            object[nameOfValue[index]] = value[index]
+        })
+        )
+    }
+
     if (calculation === null || bodyType === null){
         return (
             <>
@@ -86,18 +126,20 @@ export default function Result () {
         return (
             <Main>
                 <Index color={dataOfBodyType[bodyType]["color"]}>{calculation.toFixed(2)}</Index>
-                    <ChartContainer show={(bodyType === "Morbidly Obese" || bodyType === "") ? false : true}>
-                        <BarChart data={[{"name" : "Fatih", "value" : dataOfBodyType[bodyType]["upper"]}]} layout="vertical" width={700} height={100}>
-                            <ReferenceLine xAxisId={0} x={calculation.toFixed(2)} isFront={true} strokeWidth={3}>
-                                <Label value="You're here" position="insideTop" fill="white"/>
-                            </ReferenceLine>
-                            <YAxis dataKey="name" type="category" hide={true}/>
-                            <XAxis xAxisId={0} dataKey="value" type="number" domain={[dataOfBodyType[bodyType]["lower"], dataOfBodyType[bodyType]["upper"]]} tickCount={3} interval="preserveStartEnd" />
-                            <Bar dataKey="value" fill={dataOfBodyType[bodyType]["color"]}>
-                                <LabelList dataKey="name" position="center" formatter={(value) => bodyType} fill="white"/>
-                            </Bar>
-                        </BarChart>
-                    </ChartContainer>
+                <BodyTypeContainer>
+                    {bodyTypeAround(bodyType).map(renderBodyType)}
+                </BodyTypeContainer>
+                <ChartContainer show={(bodyType === "Morbidly Obese" || bodyType === "") ? false : true}>
+                    <BarChart data={dataProducer(bodyType)} layout="vertical" width={700} height={100}>
+                        <ReferenceLine xAxisId={0} x={calculation.toFixed(2)} isFront={true} strokeWidth={3}>
+                            <Label value="You're here" position="insideTop" fill="white"/>
+                        </ReferenceLine>
+                        <YAxis dataKey="name" type="category" hide={true}/>
+                        <XAxis xAxisId={0} dataKey="value" type="number" domain={[dataOfBodyType[bodyType]["lower"]-2, dataOfBodyType[bodyType]["upper"]+2]} tickCount={3} interval="preserveStartEnd" />
+                        <Bar dataKey="middle" fill={dataOfBodyType[bodyType]["color"]}>
+                        </Bar>
+                    </BarChart>
+                </ChartContainer>
             </Main>
         )
     }
