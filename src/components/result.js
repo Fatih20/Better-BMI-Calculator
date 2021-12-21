c5bbb562ebb447f0c9db14c666232fefee0b81c2
Fatih20 <fnri39@protonmail.com>
2021-12-21 21:49:47 +0700

    Continue work on showing result

--- src/components/result.js
+++ src/components/result.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useRef} from "react";
 import styled from "styled-components";
-import { BarChart, Bar, XAxis, ResponsiveContainer, ReferenceLine, YAxis } from "recharts";
+import { BarChart, Bar, XAxis, ResponsiveContainer, ReferenceLine, YAxis, Label, LabelList } from "recharts";
 
 import { useCalculationContext } from "./body";
 
@@ -12,7 +12,7 @@ const Main = styled.div`
 
 const Index = styled.h2`
     color : ${({color}) => color};
-    font-size: 48px;
+    font-size: 72px;
 `;
 
 const ChartContainer = styled.div`
@@ -22,56 +22,54 @@ const ChartContainer = styled.div`
 export default function Result () {
     const [calculation, setCalculation] = useCalculationContext();
     const [bodyType, setBodyType] = useState(null);
-    const roundedCalculation = useRef(null);
     console.log(calculation);
 
     useEffect(() => {
-        console.log(bodyType);
-        console.log(calculation);
+        // console.log(bodyType);
+        // console.log(calculation);
         if (calculation !== null){
             determineBodyType();
-            roundedCalculation.current = calculation.toFixed(2);
         }
     }, [calculation])
 
     function determineBodyType (){
         let newBodyType;
         if (calculation < 18.5){
-            newBodyType = "underweight"
+            newBodyType = "Underweight"
         } else if (calculation < 25){
-            newBodyType = "normal"
+            newBodyType = "Normal"
         } else if (calculation < 30){
-            newBodyType = "overweight"
+            newBodyType = "Overweight"
         } else if (calculation < 40){
-            newBodyType = "obese"
+            newBodyType = "Obese"
         } else {
-            newBodyType = "morbidly obese"
+            newBodyType = "Morbidly Obese"
         }
         setBodyType(newBodyType);
     }
 
     const dataOfBodyType = {
-        "underweight" : {
+        "Underweight" : {
             "lower" : 0,
             "upper" : 18.5,
             "color": "#64baff"
         },
-        "normal" : {
+        "Normal" : {
             "lower" : 18.5,
             "upper" : 25,
             "color": "#68b723"
         },
-        "overweight" : {
+        "Overweight" : {
             "lower" : 25,
             "upper" : 30,
             "color": "#f9c440"
         },
-        "obese" : {
+        "Obese" : {
             "lower" : 30,
             "upper" : 40,
             "color": "#f37329"
         },
-        "morbidly obese" : {
+        "Morbidly Obese" : {
             "lower" : 40,
             "upper" : 1000000000,
             "color": "#c6262e"
@@ -87,13 +85,17 @@ export default function Result () {
     } else {
         return (
             <Main>
-                <Index color={dataOfBodyType[bodyType]["color"]}>{calculation}</Index>
-                    <ChartContainer show={(bodyType === "morbidly obese" || bodyType === "") ? false : true}>
+                <Index color={dataOfBodyType[bodyType]["color"]}>{calculation.toFixed(2)}</Index>
+                    <ChartContainer show={(bodyType === "Morbidly Obese" || bodyType === "") ? false : true}>
                         <BarChart data={[{"name" : "Fatih", "value" : dataOfBodyType[bodyType]["upper"]}]} layout="vertical" width={700} height={100}>
-                            <ReferenceLine xAxisId={0} x={calculation.toFixed()}/>
+                            <ReferenceLine xAxisId={0} x={calculation.toFixed(2)} isFront={true} strokeWidth={3}>
+                                <Label value="You're here" position="insideTop" fill="white"/>
+                            </ReferenceLine>
                             <YAxis dataKey="name" type="category" hide={true}/>
-                            <XAxis xAxisId={0} dataKey="value" type="number" domain={[dataOfBodyType[bodyType]["lower"], dataOfBodyType[bodyType]["upper"]]} />
-                            <Bar dataKey="value" fill={dataOfBodyType[bodyType]["color"]}/>
+                            <XAxis xAxisId={0} dataKey="value" type="number" domain={[dataOfBodyType[bodyType]["lower"], dataOfBodyType[bodyType]["upper"]]} tickCount={3} interval="preserveStartEnd" />
+                            <Bar dataKey="value" fill={dataOfBodyType[bodyType]["color"]}>
+                                <LabelList dataKey="name" position="center" formatter={(value) => bodyType} fill="white"/>
+                            </Bar>
                         </BarChart>
                     </ChartContainer>
             </Main>
