392a656274a9082110da0502cc68d8b019c50d01
Fatih20 <fnri39@protonmail.com>
2021-12-21 16:54:42 +0700

    Barebone form

--- src/components/result.js
+++ src/components/result.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+import { useCalculationContext } from "./body";
+
 const Main = styled.div`
 `;
 
 export default function Result () {
+    const [calculation, setCalculation] = useCalculationContext();
+
     return (
         <Main>
-            
+            <h2>{calculation}</h2>
         </Main>
     )
 }
