bba048150f11cf75c69220d41307362b50206861
Fatih20 <fnri39@protonmail.com>
2021-12-25 19:28:12 +0700

    Some styling to the form though incomplete

--- src/components/form.js
+++ src/components/form.js
@@ -31,10 +31,8 @@ const StyledInput = styled.input`
     border: solid 1px black;
     border-radius: 3px;
     font-size: 18px;
-    font-family: 'Inter', sans-serif;
     height: 100%;
     padding: 5px;
-    width: 120px;
 
     &:focus {
         outline-color: black;
