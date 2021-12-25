0a32268affa6c0fc6e28713194a2ab826d7b8fa2
Fatih20 <fnri39@protonmail.com>
2021-12-25 19:06:11 +0700

    Fix reference line not being in front and add some styling to the bars

--- src/components/result.js
+++ src/components/result.js
@@ -37,6 +37,31 @@ const ChartContainer = styled.div`
     display: ${({show}) => show ? "block" : "none"};
 `;
 
+function swapPosition (array, a, b){
+    [array[a], array[b]] = [array[b], array[a]]
+}
+
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length===1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length===1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length===1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
+
 export default function Result () {
     const [calculation, setCalculation] = useCalculationContext();
     const [bodyType, setBodyType] = useState(null);
@@ -120,7 +145,6 @@ export default function Result () {
             data[0]["bottom"] = dataOfBodyType[bodyType]["lower"];
             data[0]["middle"] = dataOfBodyType[bodyType]["upper"]-dataOfBodyType[bodyType]["lower"];
             data[0]["upper"] = 2;
-
         }
 
         return data;
@@ -156,12 +180,13 @@ export default function Result () {
         const data = dataProducer()[0];
         const listOfIncludedBodyType = includedBodyTypeProducer("body type");
         let listOfBars = [];
+        console.log(data);
         Object.keys(data).forEach((position, index) => {
+            const color = dataOfBodyType[listOfIncludedBodyType[index]]["color"];
             listOfBars.push(
-                <Bar id={`${index}${position}`} stackId="a" dataKey={position} fill={dataOfBodyType[listOfIncludedBodyType[index]]["color"]} />
+                <Bar id={`${index}${position}`} stackId="a" dataKey={position} fill={color} stroke={shadeColor(color, -30)} strokeWidth={1.75}/>
             )
         })
-
         return listOfBars;
     };
 
@@ -183,12 +208,11 @@ export default function Result () {
                 </BodyTypeContainer>
                 <ChartContainer show={bodyType === "" ? false : true}>
                     <BarChart data={dataProducer()} layout="vertical" width={700} height={100}>
-                        <ReferenceLine xAxisId={0} x={calculation.toFixed(2)} isFront={true} strokeWidth={bodyType === "Morbidly Obese" ? 0 : 3}>
-                            {/* <Label value="You're here" position="insideTop" fill="white"/> */}
-                        </ReferenceLine>
                         <YAxis dataKey="name" type="category" hide={true}/>
                         <XAxis xAxisId={0} dataKey="value" type="number" domain={domainProducer()} ticks={tickProducer()} />
                         {barGenerator()}
+                        <ReferenceLine xAxisId={0} x={calculation.toFixed(2)} isFront={true} strokeWidth={bodyType === "Morbidly Obese" ? 0 : 6} stroke="black" />
+                        <ReferenceLine xAxisId={0} x={calculation.toFixed(2)} isFront={true} strokeWidth={bodyType === "Morbidly Obese" ? 0 : 3} stroke="white" />
                     </BarChart>
                 </ChartContainer>
             </Main>
